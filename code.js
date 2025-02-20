1735b54bd3df3be2506da800dc328bd5e034b2c6
Chady <chady.a@eurisko.net>
2025-02-20 11:41:32 +0200

    capitalize the letter

--- code.js
+++ code.js
@@ -7,6 +7,9 @@ function processNames() {
     .map((name) => name.replace(/^\d+\s*[-.]?\s*/, "")) // Remove numbering with "." or "-" (e.g., "1. Name" or "2 - Name")
     .map((name) => name.replace(/\s+/g, " ")) // Ensure only one space between words
     .map((name) => name.replace(/\s*\/\s*/g, " / ")) // Make sure there's 1 space before and after "/"
+    .map((name) =>
+      name.replace(/\b([a-z])\s*-\s*/g, (match, p1) => p1.toUpperCase() + " - ")
+    ) // Capitalize letter before dash
     .map((name) => name.replace(/\b([A-Za-z])\s*-\s*/g, "$1 - ")) // Correct spacing for patterns like L- or E-
     .filter((name) => name !== "") // Remove empty lines
     .sort(); // Sort alphabetically
